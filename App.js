ecde49c05ff663db8a58fa0370a5563c7ee6f1b1
tidal-t <153453762+tidal-t@users.noreply.github.com>
2024-02-20 11:55:38 +0330

    Update App.js

--- App.js
+++ App.js
@@ -1,3 +1,11 @@
-document.querySelector(".width")
-.innerHTML = document.querySelector("body")
-.ClientWidth
+// HTML
+{/* <div class="width">This is a div element with class width</div> */}
+
+// JavaScript
+document.addEventListener("DOMContentLoaded", function() {
+    var elements = document.querySelectorAll(".width");
+    elements.forEach(function(element) {
+        var width = element.offsetWidth;
+        element.innerHTML += " - Width: " + width + "px";
+    });
+});
